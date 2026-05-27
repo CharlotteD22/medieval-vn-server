81a5b797c6a9ed13b07c4e760dd01f8b80083b8d
Dulac <dulac.charlotte@hotmail.fr>
2026-05-27 21:33:03 +0200

    fix: dotenv uniquement en dev

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
 });
 
 // Générer un dialogue IA
